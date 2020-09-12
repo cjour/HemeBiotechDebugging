4d3d9992bf2aeea5bdf1670c8badbf17469ee4d8
jourc <jour.clement@gmail.com>
2020-09-12 11:18:50 +0200

    Declaration of writerSymptom interface

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+
+/**
+ * Anything that will write source from symptom data.
+ * The important part is, the return value from the operation, which is a list of strings,
+ * that may not contain many duplications
+ * 
+ * The implementation must return an orderlist
+ * 
+ */
+// Each interface must be declared in its own file.
+
+public interface ISymptomWriter {
+
+}
